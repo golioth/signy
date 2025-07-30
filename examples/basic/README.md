c8de0e110fc17c10838226127c47a1b82fa46a1e
Daniel Mangum <georgedanielmangum@gmail.com>
2025-07-30 13:52:57 -0400

    examples: add basic signing example with imported key
    Adds a basic signing example that imports key using PSA APIs.
    
    Signed-off-by: Daniel Mangum <georgedanielmangum@gmail.com>

---
+++ examples/basic/README.md
@@ -0,0 +1,115 @@
+# Basic Signing Example
+
+The basic signing example demonstrates how to use `signy` to generate a signed
+URL using an imported
+[ECDSA](https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm)
+private key.
+
+## Building & Programming
+
+The basic signing example can be built using the following command.
+
+```
+west build -p -b <board>
+```
+
+The example can be programmed on a device using the following command.
+
+```
+west flash
+```
+
+## Setup
+
+In order to successfully generate signed URLs, valid credentials must be
+configured, and the device system time must be updated.
+
+### Configuring Credentials
+
+In a production scenario, private keys should always be generated and stored in
+a secure location on a device, and certificates should be issued using [Public
+Key Infrastructure (PKI)](https://docs.golioth.io/connectivity/credentials/pki).
+
+For demonstration purposes, keys and certificates can be generated locally, then
+included in the firmware build. `signy` requires the use of a `secp256r1`
+elliptic curve key pair. If using `signy` with Golioth, see the [relevant
+documentation](https://docs.golioth.io/connectivity/credentials/pki#establishing-pki)
+for establishing a Certificate Authority (CA) and issuing device certificates.
+
+After acquiring DER-encoded private key and certificate, use the following
+[`mcumgr`](https://github.com/apache/mynewt-mcumgr) or
+[`smpmgr`](https://github.com/intercreate/smpmgr) commands on your local machine
+to load them into the filesystem on the device.
+
+Load the device certificate.
+
+```
+mcumgr --conntype serial --connstring /dev/ttyACM0 fs upload device.crt.der /lfs1/credentials/crt.der
+```
+
+**or**
+
+```
+smpmgr --port /dev/ttyACM0 --mtu 128 file upload device.crt.der /lfs1/credentials/crt.der
+```
+
+Convert device private key to `device_key.inc` in the [format
+expected](https://datatracker.ietf.org/doc/html/rfc5915.html) by
+`psa_import_key()` for a [Weierstrass Elliptic curve key
+pair](https://arm-software.github.io/psa-api/crypto/1.1/api/keys/management.html#key-formats).
+
+
+```
+tail -c +8 device.key.der | head -c +32 > device.key.psa.der
+```
+
+Load the device private key.
+
+```
+mcumgr --conntype serial --connstring /dev/ttyACM0 fs upload device.key.psa.der /lfs1/credentials/key.der
+```
+
+**or**
+
+```
+smpmgr --port /dev/ttyACM0 --mtu 128 file upload device.key.psa.der /lfs1/credentials/key.der
+```
+
+After configuring credentials, reboot the device to initialize `signy` with them.
+
+### Configuring Time
+
+In order to generate a valid signed URL with `signy`, a device must have access
+to an accurate time source. In a production scenario, [Network Time Protocol
+(NTP)](https://en.wikipedia.org/wiki/Network_Time_Protocol) or other clock
+synchronization mechanisms may be employed.
+
+For demonstration purposes, the device system time can be set in the basic
+signing example using the `date set` shell command after establishing a serial
+connection to the device. The date and time must be provided as UTC in the
+following format.
+
+```
+[Y-m-d] <H:M:S>
+```
+
+For example, the following command would set the device system time to
+Septemeber 10th, 2025 at 6:32 PM UTC.
+
+```
+date set 2025-09-10 18:32:00
+```
+
+## Generating Signed URLs
+
+After credentials and time are configured, signed URLs can be generated on the
+device using the `signy sign` shell command over a serial connection. For
+example, if using `signy` with Golioth, a signed URL can be generated for the
+`main@1.0.0` OTA artifact in your project using the following command.
+
+```
+signy sign https://gw.golioth.io/.u/c/main@1.0.0
+```
+
+The generated signed URL will be valid for the duration specified in
+`CONFIG_SIGNY_URL_VALIDITY_DURATION`.
---
+++ examples/basic/src/main.c
@@ -0,0 +1,179 @@
+/*
+ * Copyright (c) 2025 Golioth
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdlib.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/fs/fs.h>
+#include <zephyr/shell/shell.h>
+#include <psa/crypto.h>
+#include <signy/signy.h>
+
+LOG_MODULE_REGISTER(signy_basic, LOG_LEVEL_DBG);
+
+static char signed_url[CONFIG_SIGNED_URL_MAX_SIZE];
+
+#define CERT_DIR "/lfs1/credentials"
+#define CERT_FILE CERT_DIR "/crt.der"
+#define KEY_FILE CERT_DIR "/key.der"
+
+static void ensure_credentials_dir(void)
+{
+    struct fs_dirent dirent;
+
+    int err = fs_stat(CERT_DIR, &dirent);
+    if (err == -ENOENT)
+    {
+        err = fs_mkdir(CERT_DIR);
+    }
+
+    if (err)
+    {
+        LOG_ERR("Failed to create credentials dir: %d", err);
+    }
+}
+
+static ssize_t get_file_size(const char *path)
+{
+    struct fs_dirent dirent;
+
+    int err = fs_stat(path, &dirent);
+
+    if (err < 0)
+    {
+        return err;
+    }
+    if (dirent.type != FS_DIR_ENTRY_FILE)
+    {
+        return -EISDIR;
+    }
+
+    return dirent.size;
+}
+
+static ssize_t read_file(const char *path, uint8_t **out)
+{
+    // Ensure that the credentials directory exists, so the user doesn't have to
+    ensure_credentials_dir();
+
+    ssize_t size = get_file_size(path);
+    if (size <= 0)
+    {
+        return size;
+    }
+
+    uint8_t *buf = malloc(size);
+    if (buf == NULL)
+    {
+        return -ENOMEM;
+    }
+
+    struct fs_file_t file;
+    fs_file_t_init(&file);
+
+    int err = fs_open(&file, path, FS_O_READ);
+    if (err)
+    {
+        LOG_ERR("Could not open %s, err: %d", path, err);
+        free(buf);
+        return err;
+    }
+
+    size = fs_read(&file, buf, size);
+    if (size < 0)
+    {
+        LOG_ERR("Could not read %s, err: %d", path, size);
+        free(buf);
+        goto finish;
+    }
+
+    LOG_DBG("Read %d bytes from %s", size, path);
+
+    *out = buf;
+
+finish:
+    fs_close(&file);
+    return size;
+}
+
+static int cmd_signy_sign(const struct shell *shell, size_t argc, char *argv[])
+{
+    size_t signed_url_len;
+    int err =
+        signy_sign_url(argv[1], strlen(argv[1]), signed_url, sizeof(signed_url), &signed_url_len);
+    if (err != 0)
+    {
+        shell_error(shell, "Failed to sign URL: %d", err);
+        return -1;
+    }
+
+    shell_print(shell, "Signed URL (len: %d): %s", signed_url_len, signed_url);
+
+    return 0;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+    sub_signy,
+    SHELL_CMD_ARG(sign, NULL, "Generate signed URL.", cmd_signy_sign, 2, 0),
+    SHELL_SUBCMD_SET_END);
+
+SHELL_CMD_REGISTER(signy, &sub_signy, "signy commands", NULL);
+
+int main(void)
+{
+    int err;
+    psa_status_t status;
+
+    status = psa_crypto_init();
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("Failed to initialize PSA crypto: %d", status);
+        return -1;
+    }
+
+    psa_key_attributes_t attributes = PSA_KEY_ATTRIBUTES_INIT;
+
+    psa_set_key_usage_flags(&attributes, PSA_KEY_USAGE_SIGN_HASH);
+    psa_set_key_lifetime(&attributes, PSA_KEY_LIFETIME_VOLATILE);
+    psa_set_key_algorithm(&attributes, PSA_ALG_ECDSA(PSA_ALG_SHA_256));
+    psa_set_key_type(&attributes, PSA_KEY_TYPE_ECC_KEY_PAIR(PSA_ECC_FAMILY_SECP_R1));
+    psa_set_key_bits(&attributes, 256);
+
+    psa_key_id_t priv_key;
+
+    uint8_t *device_key_der;
+    ssize_t device_key_der_len = read_file(KEY_FILE, &device_key_der);
+    if (device_key_der_len < 0)
+    {
+        LOG_ERR("Failed to read device key file: %d", device_key_der_len);
+        return -1;
+    }
+
+    status = psa_import_key(&attributes, device_key_der, device_key_der_len, &priv_key);
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("Failed to import private key: %d", status);
+        return -1;
+    }
+
+    free(device_key_der);
+    psa_reset_key_attributes(&attributes);
+
+    uint8_t *device_crt_der;
+    ssize_t device_crt_der_len = read_file(CERT_FILE, &device_crt_der);
+    if (device_crt_der_len < 0)
+    {
+        LOG_ERR("Failed to read device certificate file: %d", device_crt_der_len);
+        return -1;
+    }
+
+    err = signy_init(priv_key, device_crt_der, device_crt_der_len);
+    if (err != 0)
+    {
+        LOG_ERR("Failed to initialize signy: %d", err);
+        return -1;
+    }
+}
